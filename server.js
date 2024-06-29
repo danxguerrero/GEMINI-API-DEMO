12b5934f33be24ae5c64f12b4e3947e6322a3c2d
danxguerrero <danxguerrero@yahoo.com>
2024-06-29 01:38:56 +0000

    Show response in terminal for a Gemini API

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const { GoogleGenerativeAI } = require('@google/generative-ai')
 
 const API_KEY = process.env.API_KEY
 
-const genAI = new GoogleGenerativeAI(API_KEY)
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-const testQuery = async () => {
-  const prompt = 'Can you generate a leg day workout?'
+  const prompt = "Create a five day workout plan for me focused on burning fat."
 
-  const result = await model.generateContent(prompt)
-
-  console.log(result)
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
 }
 
-testQuery()
+run();
